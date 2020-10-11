74989a433d0bfdcbb334edfb241766d0e36737a9
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-10-11 18:41:41 +0300

    feat(add): project constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export * from './agent'
+export * from './store'
+export * from './prefix'
---
+++ src/constants/prefix.ts
@@ -0,0 +1,7 @@
+export const PREFIX = {
+    UUID: 'data-nooa-uuid',
+    HOST: 'data-nooa-host',
+    NO_TRACK: 'data-nooa-no-track',
+    TAG: 'data-nooa-tag',
+    TYPE: 'data-nooa-type',
+}
